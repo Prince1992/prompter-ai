437f9ecc824ab775dd7a0e2bdc670ad79d507d01
Amar Singh Yadav <amarprince9@gmail.com>
2023-07-03 12:51:33 +0530

    Implimented Create Prompt

--- app/create-prompt/page.js
+++ app/create-prompt/page.js
@@ -1,9 +1,48 @@
-import React from 'react'
+'use client';
+import { useState } from 'react';
+import { useSession } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
+import Form from '@components/Form';
 
 const CreatePropmt = () => {
-  return (
-    <div>Create Prompts</div>
-  )
-}
+  const router = useRouter();
+  const { data: session } = useSession();
+  const [submitting, setSubmitting] = useState(false);
+  const [post, setPost] = useState({
+    prompt: '',
+    tag: '',
+  });
 
-export default CreatePropmt
+  const createPrompt = async (e) => {
+    e.preventDefault();
+    setSubmitting(true);
+    try {
+      const response = await fetch('/api/prompt/new', {
+        method: 'POST',
+        body: JSON.stringify({
+          prompt: post.prompt,
+          tag: post.tag,
+          userId: session?.user.id,
+        }),
+      });
+      if (response.ok) {
+        router.push('/');
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setSubmitting(false);
+    }
+  };
+  return (
+    <Form
+      type="create"
+      post={post}
+      setPost={setPost}
+      submitting={submitting}
+      handldleSubmit={createPrompt}
+    />
+  );
+};
+
+export default CreatePropmt;

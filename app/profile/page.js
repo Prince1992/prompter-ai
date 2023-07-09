f50290fc9b78c42dd02598881170fcc89151ac13
Amar Singh Yadav <amarprince9@gmail.com>
2023-07-09 21:03:13 +0530

    Implemented Delete Promts

--- app/profile/page.js
+++ app/profile/page.js
@@ -6,24 +6,37 @@ import Profile from '@components/Profile';
 const MyProfile = () => {
   const router = useRouter();
   const { data: session } = useSession();
-  const [mypost, seMyPost] = useState([]);
+  const [posts, setPosts] = useState([]);
   useEffect(() => {
     const fetchPost = async () => {
       const response = await fetch(`/api/users/${session?.user.id}/posts`);
       const data = await response.json();
-      seMyPost(data);
+      setPosts(data);
     };
     if (session?.user.id) fetchPost();
   }, [session?.user.id]);
   const handleEdit = (post) => {
     router.push(`/update-prompt?id=${post._id}`);
   };
-  const handleDelete = async (post) => {};
+  const handleDelete = async (post) => {
+    const hasConfirmed = confirm(
+      'Are you sure you want to delete this prompt?'
+    );
+    if (hasConfirmed) {
+      try {
+        await fetch(`/api/prompt/${post._id.toString()}`, {
+          method: 'DELETE',
+        });
+        const filteredPosts = posts.filter((p) => p._id !== post._id);
+        setPosts(filteredPosts);
+      } catch (error) {}
+    }
+  };
   return (
     <Profile
       name="Amar"
       desc="Welcome to your personlaize page"
-      data={mypost}
+      data={posts}
       handleEdit={handleEdit}
       handleDelete={handleDelete}
     />
